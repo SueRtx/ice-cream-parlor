416104fbc209b12c8a421a9a8089346a359272dd
Sue Roberts <suerobertstx@yahoo.com>
2022-10-21 23:01:13 -0500

    Add styles to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from "react";
 import iceCreamImage from "./../img/ice.png";
+import '../App.css';
 
 function Header(){
+
+  const headerStyles = {
+    textAlign: 'center',
+    letterSpacing: '3px',
+    fontSize: '20px',
+    color: 'rgb(124, 233, 139)',
+    textShadow: '3px 3px 3px #090000',
+  }
+
+  const iceStyles = {
+    color: 'rgb(208, 46, 34)',
+    textShadow: '3px 3px 3px rgb(124, 233, 139)',
+  }
+
   return (
     <React.Fragment>
-      <h1>Sunny Ice Cream</h1>
-      <img src={iceCreamImage} alt="Various scoops of ice creams" width='50%'/>
+      <div style={headerStyles}>
+        <h1>SUNNY <strong style={iceStyles}>ICE CREAM</strong> PARLOR</h1>
+        <img src={iceCreamImage} alt="Various scoops of ice creams" width='50%'/>
+      </div>
     </React.Fragment>
   );
 }
